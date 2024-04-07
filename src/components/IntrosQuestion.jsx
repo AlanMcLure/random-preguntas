aa98c19caac01156fe03f5cf585d95695efcdddd
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-04-07 23:58:22 +0200

    componente intros

--- src/components/IntrosQuestion.jsx
+++ src/components/IntrosQuestion.jsx
@@ -1,15 +1,25 @@
 import PropTypes from "prop-types";
 
-function IntrosQuestion({ question }) {
+function IntrosQuestion({ question, introUrl }) {
   return (
     <div className="question-container mb-8">
       <p className="text-2xl">{question}</p>
+      <iframe
+        title="Intro"
+        width="300"
+        height="150"
+        src={introUrl}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
 
 IntrosQuestion.propTypes = {
-  question: PropTypes.string.isRequired
+  question: PropTypes.string.isRequired,
+  introUrl: PropTypes.string.isRequired
 };
 
 export default IntrosQuestion;
