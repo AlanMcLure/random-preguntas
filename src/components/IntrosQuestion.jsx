85887ac29a7733714e6a6b5c961a1c82c8675eb1
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-04-09 23:58:47 +0200

    componente audio en IntrosQuestion

--- src/components/IntrosQuestion.jsx
+++ src/components/IntrosQuestion.jsx
@@ -1,18 +1,11 @@
 import PropTypes from "prop-types";
+import Audio from "./Audio";
 
 function IntrosQuestion({ question, introUrl }) {
   return (
     <div className="question-container mb-8">
       <p className="text-2xl">{question}</p>
-      <iframe
-        title="Intro"
-        width="300"
-        height="150"
-        src={introUrl}
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-      ></iframe>
+      <Audio videoId={introUrl} />
     </div>
   );
 }
