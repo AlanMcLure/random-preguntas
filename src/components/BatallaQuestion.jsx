3c4a8cb3a1203ab1fa7bf3d99c7b6f6f4905944c
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-04-15 23:49:32 +0200

    contador que se puede reiniciar

--- src/components/BatallaQuestion.jsx
+++ src/components/BatallaQuestion.jsx
@@ -1,19 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from 'react';
 import Timer from "./Timer";
 
 function BatallaQuestion({ question, resetTimer }) {
+  const [resetFlag, setResetFlag] = useState(false);
+
+  const handleReset = () => {
+    resetTimer();
+    setResetFlag(!resetFlag);
+  };
+
   return (
     <div className="question-container mb-8">
       <p className="text-2xl">{question}</p>
-      <Timer />
-      <button onClick={resetTimer} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Reiniciar Timer</button>
+      <Timer key={resetFlag} /> {/* Agregar key para reiniciar el componente Timer */}
+      <button onClick={handleReset} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Reiniciar Timer</button>
     </div>
   );
 }
 
 BatallaQuestion.propTypes = {
   question: PropTypes.string.isRequired,
-  resetTimer: PropTypes.func.isRequired
+  resetTimer: PropTypes.func.isRequired // Se espera que resetTimer sea una función
 };
 
 export default BatallaQuestion;
